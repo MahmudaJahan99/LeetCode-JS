cb54d2733f4b9e432a47dd105e31131fd14a04b7
DepressedPotatok <mj.mjahan045@gmail.com>
2026-06-09 17:36:48 +0600

    to be or not to be

---
+++ 2704 To Be Or Not To Be.js
@@ -0,0 +1,16 @@
+var expect = function (val) {
+  return {
+    toBe: function (val2) {
+      if (val === val2) {
+        return true;
+      }
+      throw new Error("Not Equal");
+    },
+    notToBe: function (val2) {
+      if (val !== val2) {
+        return true;
+      }
+      throw new Error("Equal");
+    },
+  };
+};
